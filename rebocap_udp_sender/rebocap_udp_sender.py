b5db73636219800622b19273a0eaf4bf4629023e
labor <3110379921@qq.com>
2026-02-04 12:24:34 +0800

    fix slerp

--- rebocap_udp_sender/rebocap_udp_sender.py
+++ rebocap_udp_sender/rebocap_udp_sender.py
@@ -13,6 +13,7 @@ import json
 import sys
 import signal
 from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Slerp
 from contants import LEFT_JOINTS_MIN_VALUE, LEFT_JOINTS_MAX_VALUE, RIGHT_JOINTS_MIN_VALUE, RIGHT_JOINTS_MAX_VALUE
 from shoulder_solver import IncrementalShoulderSolver
 from gripper_controller import GripperController
@@ -31,7 +32,7 @@ class ReboCapUdpSender:
         self.print_interval = 60
 
         # Send thread parameters
-        self.send_freq = 180  # Hz
+        self.send_freq = 500  # Hz
         self.dt = 1.0 / self.send_freq
 
         # Pose storage for interpolation
@@ -40,6 +41,7 @@ class ReboCapUdpSender:
         self.cur_pose = None
         self.cur_timestamp = None
         self.pose_lock = threading.Lock()
+        self.key_times = [0, 1]
 
         # Create UDP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -86,16 +88,11 @@ class ReboCapUdpSender:
         """Callback when new pose data is received from RoboCap"""
         try:
             with self.pose_lock:
-                self.cur_pose = self.last_pose
-                self.cur_timestamp = self.last_timestamp
-                self.last_pose = deepcopy(pose24)  # Deep copy
-                self.last_timestamp = ts
-
-            # Print status every 60 frames (~1 second at 60Hz)
-            self.total_updates += 1
-            # if self.debug and self.total_updates % self.print_interval == 0:
-            #     print(f"Received pose at timestamp: {ts}")
-
+                now = time.time()
+                self.last_pose = self.cur_pose
+                self.last_timestamp = self.cur_timestamp
+                self.cur_pose = deepcopy(pose24)  # Deep copy
+                self.cur_timestamp = now
         except Exception as e:
             print(f"❌ Error in pose callback: {e}")
     
@@ -127,10 +124,11 @@ class ReboCapUdpSender:
                 last_ts = self.last_timestamp
 
             if cur_pose is not None and last_pose is not None and cur_ts is not None and last_ts is not None:
-                duration = last_ts - cur_ts
+                duration = cur_ts - last_ts
                 if duration > 0:
-                    alpha = min(1.0, (now - cur_ts) / duration)
-                    interp_pose = self.interpolate_pose(cur_pose, last_pose, alpha)
+                    alpha = min(1.0, (now - duration - last_ts) / duration)
+                    alpha = max(0.0, alpha)
+                    interp_pose = self.interpolate_pose(last_pose, cur_pose, alpha)
                     joint_angles = self.map_to_openarm_joints_interp(interp_pose)
                     self.send_udp(now, joint_angles)
 
@@ -141,18 +139,16 @@ class ReboCapUdpSender:
         interp_pose = []
         # Indices of interest: -11 collar, -8 shoulder, -6 elbow, -4 wrist for left; -10, -7, -5, -3 for right
         indices = [-11, -8, -6, -4, -10, -7, -5, -3]
-        for i in range(len(pose1)):
+        for i in range(-24, 0, 1):
             if i in indices:
-                q1 = np.array(pose1[i])
-                q2 = np.array(pose2[i])
-                r1 = R.from_quat(q1, scalar_first=False)
-                r2 = R.from_quat(q2, scalar_first=False)
-                interp_r = R.slerp(r1, r2, alpha)
+                key_rots = R.from_quat([pose1[i], pose2[i]], scalar_first=False)
+                slerp = Slerp(self.key_times, key_rots)
+                interp_r = slerp(alpha)
                 interp_q = interp_r.as_quat(scalar_first=False)
                 interp_pose.append(interp_q.tolist())
             else:
                 # For other joints, linear interpolation or copy
-                interp_pose.append(pose1[i][:])  # Copy pose1 for now
+                interp_pose.append(pose2[i][:])  # Copy pose2 for now
         return interp_pose
 
     def map_to_openarm_joints_interp(self, pose24):
@@ -234,93 +230,13 @@ class ReboCapUdpSender:
                   r_j1, r_j2, r_j3, r_j4, r_j5, r_j6, r_j7]
         joints = np.clip(joints, LEFT_JOINTS_MIN_VALUE+RIGHT_JOINTS_MIN_VALUE, LEFT_JOINTS_MAX_VALUE+RIGHT_JOINTS_MAX_VALUE).tolist()
 
-        return joints
-
-    #--------------------Developing-----------------
-    def map_to_openarm_joints(self, pose24):
-        """
-        Map RoboCap quaternions to OpenArm 7-DOF joint angles        
-        """
-        ## left        
-        left_collar = pose24[-11]
-        left_shoulder = pose24[-8]
-        left_elbow = pose24[-6]
-        left_wrist = pose24[-4]
-
-        ## right
-        right_collar = pose24[-10]
-        right_shoulder = pose24[-7]
-        right_elbow = pose24[-5]
-        right_wrist = pose24[-3]
-
-        ## Fuse collar rotation and shoulder rotation
-        left_shoulder = (R.from_quat(left_collar) * R.from_quat(left_shoulder)).as_quat()
-        right_shoulder = (R.from_quat(right_collar) * R.from_quat(right_shoulder)).as_quat()        
-
-
-        ## ----------------For left arm--------------------
-        if (not self.l_shoulder_initialized):
-            # Euer angle decomposition
-            l_shoulder = R.from_quat(left_shoulder, scalar_first=False)
-            l_j1, l_j2, l_j3 = l_shoulder.as_euler('XYX')
-            l_j2 -= np.pi / 2.0  # Adjust for OpenArm's
-            self.l_shoulder_solver.init_joints([l_j1, l_j2, l_j3])
-            self.l_shoulder_initialized = True
-        else:
-            l_j1, l_j2, l_j3 = self.l_shoulder_solver.solve(left_shoulder)
-        # Just use Euler angles for elbow and wrist
-        l_elbow = R.from_quat(left_elbow, scalar_first=False)
-        z, _, x = l_elbow.as_euler('ZYX')
-        l_j4 = -z
-        l_j5 = x
-        l_wrist = R.from_quat(left_wrist, scalar_first=False)
-        z, y, _ = l_wrist.as_euler('ZYX')
-        l_j6 = -y
-        l_j7 = z
-
-
-        ## ----------------For right arm--------------------
-        if (not self.r_shoulder_initialized):
-            # Euler angle decomposition
-            r_shoulder = R.from_quat(right_shoulder, scalar_first=False)
-            r_j1, r_j2, r_j3 = r_shoulder.as_euler('XYX')
-            if r_j1 > 0:
-                r_j1 -= np.pi
-            else:
-                r_j1 += np.pi
-            r_j1 = -r_j1
-            r_j2 -= np.pi / 2.0
-            r_j2 = -r_j2
-            if r_j3 > 0:
-                r_j3 -= np.pi
-            else:
-                r_j3 += np.pi
-            r_j3 = -r_j3
-            self.r_shoulder_solver.init_joints([r_j1, r_j2, r_j3])
-            self.r_shoulder_initialized = True
-        else:
-            r_j1, r_j2, r_j3 = self.r_shoulder_solver.solve(right_shoulder)
-                
-        ## Elbow and wrist (for right arm)
-        r_elbow = R.from_quat(right_elbow, scalar_first=False)
-        z, _, x = r_elbow.as_euler('ZYX')
-        r_j4 = z
-        r_j5 = -x - (np.pi/4)
-        r_wrist = R.from_quat(right_wrist, scalar_first=False)
-        z, y, _ = r_wrist.as_euler('ZYX')
-        r_j6 = -z
-        r_j7 = -y
-
-        joints = [l_j1, l_j2, l_j3, l_j4, l_j5, l_j6, l_j7,
-                  r_j1, r_j2, r_j3, r_j4, r_j5, r_j6, r_j7]
-        joints = np.clip(joints, LEFT_JOINTS_MIN_VALUE+RIGHT_JOINTS_MIN_VALUE, LEFT_JOINTS_MAX_VALUE+RIGHT_JOINTS_MAX_VALUE).tolist()
-
+        self.total_updates += 1
         if self.debug and self.total_updates % self.print_interval == 0:
             print(f"Debug Left Joints(Not clip): {l_j1:.3f}, {l_j2:.3f}, {l_j3:.3f}, {l_j4:.3f}, {l_j5:.3f}, {l_j6:.3f}, {l_j7:.3f}")
             print(f"Debug Left Joints(Clipped): {joints[0]:.3f}, {joints[1]:.3f}, {joints[2]:.3f}, {joints[3]:.3f}, {joints[4]:.3f}, {joints[5]:.3f}, {joints[6]:.3f}")
             print(f"Debug Right Joints(Not clip): {r_j1:.3f}, {r_j2:.3f}, {r_j3:.3f}, {r_j4:.3f}, {r_j5:.3f}, {r_j6:.3f}, {r_j7:.3f}")
             print(f"Debug Right Joints(Clipped): {joints[7]:.3f}, {joints[8]:.3f}, {joints[9]:.3f}, {joints[10]:.3f}, {joints[11]:.3f}, {joints[12]:.3f}, {joints[13]:.3f}")
-
+        
         return joints
 
     
